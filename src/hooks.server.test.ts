e323424cfdb7ff976d4baa86e8f6a8b2f115038d
Carl Lyons <code@carledwardlyons.ca>
2025-08-08 14:28:07 -0400

    Ignore Client Errors During Error Handling
    When an HTTP error occurs with a client error status code (4XX), do not
    log event as an error. The event will still be logged as expected HTTP
    traffic with the respective error status.

--- src/hooks.server.test.ts
+++ src/hooks.server.test.ts
@@ -122,6 +122,16 @@ describe('error handler', () => {
     expect(sentryHandlerSpy).toHaveBeenCalledWith({ error, event });
   });
 
+  it('ignores client errors', () => {
+    const error = stubError();
+    const event = stubEvent();
+    const status = 404;
+
+    handleError({ error, event, status });
+
+    expect(log).not.toHaveBeenCalled();
+  });
+
   it('logs errors', () => {
     const error = stubError();
     const event = stubEvent();
