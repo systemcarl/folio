systemcarl/folio-deploy

Deploy Remote Resources
Provision remote resources on Cloudflare and DigitalOcean using
Terraform. Configure Cloudflare DNS and create a DigitalOcean Droplet
running the containerized application behind a *Caddy* server. Extend
helper scripts for deployment and destruction of resources, by default.

cli/tests/deploy.bats
@@ -9,6 +9,8 @@ setup() {
     bats_load_library bats-assert
     setup_mocks
 
+    mock terraform
+
     docker() {
         log_mock_call docker "$@"
         if [[ "$1" == "image" && "$2" == "inspect" ]]; then
@@ -25,18 +27,30 @@ setup() {
     export FOLIO_GH_PACKAGE="default-package"
 }
 
+setup_remote_env() {
+    export ENVIRONMENT="production"
+    export FOLIO_GH_NAMESPACE="default-namespace"
+    export FOLIO_GH_PACKAGE="default-package"
+    export FOLIO_APP_DOMAIN="example.com"
+    export FOLIO_CF_DNS_ZONE="abc123"
+    export FOLIO_SSH_PORT="22"
+    export FOLIO_PUBLIC_KEY_FILE="/path/to/public_key.pub"
+    export FOLIO_CF_TOKEN="cf_token"
+    export FOLIO_DO_TOKEN="do_token"
+}
+
 teardown() {
     teardown_mocks
 }
 
 @test "loads environment" {
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once load_env
 }
 
 @test "deploys locally" {
-    run deploy
+    run deploy --local
     assert_success
     assert_output --partial \
         "Application is running on http://localhost:3000"
@@ -44,7 +58,7 @@ teardown() {
 
 @test "deploys local Docker image to local container" {
     set_mock_state has_image true
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
@@ -54,7 +68,7 @@ teardown() {
 
 @test "deploys package override to local container" {
     set_mock_state has_image true
-    run deploy --package "test-package"
+    run deploy --local --package "test-package"
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
@@ -64,7 +78,7 @@ teardown() {
 
 @test "does not pull remote Docker image if local image exists" {
     set_mock_state has_image true
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_not_called docker pull
 }
@@ -72,7 +86,7 @@ teardown() {
 @test "deploys remote Docker image to local container requires namespace" {
     unset FOLIO_GH_NAMESPACE
     set_mock_state has_image false
-    run deploy
+    run deploy --local
     assert_failure
     assert_output --partial \
         "Error: GitHub namespace is required to retrieve image."
@@ -80,7 +94,7 @@ teardown() {
 
 @test "pulls remote Docker image if local image does not exist" {
     set_mock_state has_image false
-    run deploy
+    run deploy  --local
     assert_success
     assert_mock_called_once docker pull \
         "ghcr.io/default-namespace/default-package:latest"
@@ -88,7 +102,7 @@ teardown() {
 
 @test "pulls remote Docker image from namespace override" {
     set_mock_state has_image false
-    run deploy --namespace "test-namespace"
+    run deploy --local --namespace "test-namespace"
     assert_success
     assert_mock_called_once docker pull \
         "ghcr.io/test-namespace/default-package:latest"
@@ -96,7 +110,7 @@ teardown() {
 
 @test "pulls remote Docker image from package override" {
     set_mock_state has_image false
-    run deploy --package "test-package"
+    run deploy --local --package "test-package"
     assert_success
     assert_mock_called_once docker pull \
         "ghcr.io/default-namespace/test-package:latest"
@@ -104,7 +118,7 @@ teardown() {
 
 @test "pulls remote Docker image before deploying" {
     set_mock_state has_image false
-    run deploy
+    run deploy  --local
     assert_success
     assert_mocks_called_in_order \
         docker pull "ghcr.io/default-namespace/default-package:latest" -- \
@@ -116,7 +130,7 @@ teardown() {
 
 @test "deploys remote Docker image to local container" {
     set_mock_state has_image false
-    run deploy
+    run deploy  --local
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
@@ -126,7 +140,7 @@ teardown() {
 
 @test "deploys remote Docker image to local container with namespace override" {
     set_mock_state has_image false
-    run deploy --namespace "test-namespace"
+    run deploy --local --namespace "test-namespace"
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
@@ -136,10 +150,202 @@ teardown() {
 
 @test "deploys remote Docker image to local container with package override" {
     set_mock_state has_image false
-    run deploy --package "test-package"
+    run deploy --local --package "test-package"
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
         -p "3000:3000" \
         "ghcr.io/default-namespace/test-package:latest"
+}
+
+@test "deploys remotely" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "requires namespace to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_GH_NAMESPACE
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: GitHub namespace required for non-local deployment."
+}
+
+@test "requires domain to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_APP_DOMAIN
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Domain required for non-local deployment."
+}
+
+@test "requires Cloudflare DNS zone to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_CF_DNS_ZONE
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Cloudflare DNS zone required for non-local deployment."
+}
+
+@test "requires public key file to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_PUBLIC_KEY_FILE
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Public key file required for non-local deployment."
+}
+
+@test "requires Cloudflare API token to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_CF_TOKEN
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Cloudflare token required for non-local deployment."
+}
+
+@test "requires DigitalOcean API token to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_DO_TOKEN
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: DigitalOcean token required for non-local deployment."
+}
+
+@test "accepts namespace as option" {
+    setup_remote_env
+    unset FOLIO_GH_NAMESPACE
+    run deploy <<< "y" --namespace "test-namespace"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts domain as option" {
+    setup_remote_env
+    unset FOLIO_APP_DOMAIN
+    run deploy <<< "y" --domain "example.com"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts Cloudflare DNS zone as option" {
+    setup_remote_env
+    unset FOLIO_CF_DNS_ZONE
+    run deploy <<< "y" --dns-zone "example.com"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts public key file as option" {
+    setup_remote_env
+    unset FOLIO_PUBLIC_KEY_FILE
+    run deploy <<< "y" --public-key "/path/to/public_key.pub"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts Cloudflare API token as option" {
+    setup_remote_env
+    unset FOLIO_CF_TOKEN
+    run deploy <<< "y" --cf-token "cf_token"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts DigitalOcean API token as option" {
+    setup_remote_env
+    unset FOLIO_DO_TOKEN
+    run deploy <<< "y" --do-token "do_token"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "initializes Terraform" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once terraform init
+}
+
+@test "initializes Terraform before creating plan" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mocks_called_in_order \
+        terraform init -- \
+        terraform plan -out=tfplan
+}
+
+@test "creates Terraform plan from environment variables" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once terraform plan \
+        -out=tfplan \
+        -var "namespace=default-namespace" \
+        -var "domain=example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates Terraform plan from options" {
+    run deploy <<< "y" \
+        --namespace "test-namespace" \
+        --domain "example.com" \
+        --dns-zone "abc123" \
+        --ssh-port "2222" \
+        --public-key "/path/to/public_key.pub" \
+        --cf-token "cf_token" \
+        --do-token "do_token"
+    assert_success
+    assert_mock_called_once terraform plan \
+        -out=tfplan \
+        -var "namespace=test-namespace" \
+        -var "domain=example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=2222" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates Terraform plan before applying" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mocks_called_in_order \
+        terraform plan -out=tfplan -- \
+        terraform apply tfplan
+}
+
+@test "applies Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once terraform apply tfplan
+}
+
+@test "aborts if plan not approved" {
+    setup_remote_env
+    run deploy <<< "n"
+    assert_success
+    assert_output --partial "Deployment aborted."
+    assert_mock_not_called terraform apply
+}
+
+@test "automatically approves plan" {
+    setup_remote_env
+    run deploy --approve
+    assert_success
+    assert_mock_called_once terraform apply tfplan
 }

systemcarl/folio

cli/tests/deploy.bats
@@ -9,6 +9,8 @@ setup() {
     bats_load_library bats-assert
     setup_mocks
 
+    mock terraform
+
     docker() {
         log_mock_call docker "$@"
         if [[ "$1" == "image" && "$2" == "inspect" ]]; then
@@ -24,26 +26,39 @@ setup() {
     FOLIO_CICD_REPO="cicd-repo"
 }
 
+setup_remote_env() {
+    ENVIRONMENT="production"
+    GOOGLE_CREDENTIALS="/path/to/gcs_creds.json"
+    FOLIO_APP_DOMAIN="example.com"
+    FOLIO_CF_DNS_ZONE="abc123"
+    FOLIO_SSH_PORT="22"
+    FOLIO_SSH_KEY_ID="1234"
+    FOLIO_ACME_EMAIL="example@example.com"
+    FOLIO_PUBLIC_KEY_FILE="/path/to/public_key.pub"
+    FOLIO_CF_TOKEN="cf_token"
+    FOLIO_DO_TOKEN="do_token"
+}
+
 teardown() {
     teardown_mocks
 }
 
 @test "deploys locally" {
-    run deploy
+    run deploy --local
     assert_success
     assert_output --partial \
         "Application is running on http://localhost:3000"
 }
 
 @test "loads environment" {
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once load_env
 }
 
 @test "deploys local Docker image to local container" {
     set_mock_state has_image true
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
@@ -54,7 +69,7 @@ teardown() {
 @test "deploys local package" {
     FOLIO_CICD_REPO="test-package"
     set_mock_state has_image true
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker run -d \
         "test-package:latest"
@@ -62,14 +77,14 @@ teardown() {
 
 @test "does not pull remote Docker image if local image exists" {
     set_mock_state has_image true
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_not_called docker pull
 }
 
 @test "pulls remote Docker image if local image does not exist" {
     set_mock_state has_image false
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker pull \
         "ghcr.io/cicd-account/cicd-repo:latest"
@@ -78,7 +93,7 @@ teardown() {
 @test "pulls remote Docker image from namespace" {
     FOLIO_CICD_ACCOUNT="test-account"
     set_mock_state has_image false
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker pull \
         "ghcr.io/test-account/cicd-repo:latest"
@@ -87,7 +102,7 @@ teardown() {
 @test "pulls remote package" {
     FOLIO_CICD_REPO="test-package"
     set_mock_state has_image false
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker pull \
         "ghcr.io/cicd-account/test-package:latest"
@@ -95,7 +110,7 @@ teardown() {
 
 @test "pulls remote Docker image before deploying" {
     set_mock_state has_image false
-    run deploy
+    run deploy --local
     assert_success
     assert_mocks_called_in_order \
         docker pull "ghcr.io/cicd-account/cicd-repo:latest" -- \
@@ -107,7 +122,7 @@ teardown() {
 
 @test "deploys remote Docker image to local container" {
     set_mock_state has_image false
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
@@ -119,10 +134,255 @@ teardown() {
     FOLIO_CICD_REPO="test-package"
     FOLIO_CICD_ACCOUNT="test-account"
     set_mock_state has_image false
-    run deploy
+    run deploy --local
     assert_success
     assert_mock_called_once docker run -d \
         --name "folio" \
         -p "3000:3000" \
         "ghcr.io/test-account/test-package:latest"
+}
+
+@test "deploys remotely" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "requires domain to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_APP_DOMAIN
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Domain required for non-local deployment."
+}
+
+@test "requires Cloudflare DNS zone to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_CF_DNS_ZONE
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Cloudflare DNS zone required for non-local deployment."
+}
+
+@test "requires SSH port to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_SSH_PORT
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: SSH port required for non-local deployment."
+}
+
+@test "requires SSH key ID to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_SSH_KEY_ID
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: SSH key ID required for non-local deployment."
+}
+
+@test "requires public key file to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_PUBLIC_KEY_FILE
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Public key file required for non-local deployment."
+}
+
+@test "requires Google Cloud Service credentials to deploy remotely" {
+    setup_remote_env
+    unset GOOGLE_CREDENTIALS
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: GCS credentials required for non-local deployment."
+}
+
+@test "requires Cloudflare API token to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_CF_TOKEN
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: Cloudflare token required for non-local deployment."
+}
+
+@test "requires DigitalOcean API token to deploy remotely" {
+    setup_remote_env
+    unset FOLIO_DO_TOKEN
+    run deploy <<< "y"
+    assert_failure
+    assert_output --partial \
+        "Error: DigitalOcean token required for non-local deployment."
+}
+
+@test "accepts domain as option" {
+    setup_remote_env
+    unset FOLIO_APP_DOMAIN
+    run deploy <<< "y" --domain "example.com"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts Cloudflare DNS zone as option" {
+    setup_remote_env
+    unset FOLIO_CF_DNS_ZONE
+    run deploy <<< "y" --dns-zone "example.com"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts SSH port as option" {
+    setup_remote_env
+    unset FOLIO_SSH_PORT
+    run deploy <<< "y" --ssh-port "2222"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts SSH key ID as option" {
+    setup_remote_env
+    unset FOLIO_SSH_KEY_ID
+    run deploy <<< "y" --ssh-key-id "1234"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts public key file as option" {
+    setup_remote_env
+    unset FOLIO_PUBLIC_KEY_FILE
+    run deploy <<< "y" --public-key "/path/to/public_key.pub"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts Google Cloud Service credentials as option" {
+    setup_remote_env
+    unset GOOGLE_CREDENTIALS
+    run deploy <<< "y" --gcs-creds "/path/to/gcs_creds.json"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts Cloudflare API token as option" {
+    setup_remote_env
+    unset FOLIO_CF_TOKEN
+    run deploy <<< "y" --cf-token "cf_token"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "accepts DigitalOcean API token as option" {
+    setup_remote_env
+    unset FOLIO_DO_TOKEN
+    run deploy <<< "y" --do-token "do_token"
+    assert_success
+    assert_output --partial "Deployment of production completed successfully."
+}
+
+@test "initializes Terraform" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra init \
+        -reconfigure \
+        -backend-config="prefix=production"
+}
+
+@test "initializes Terraform before creating plan" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mocks_called_in_order \
+        terraform -chdir=infra init \
+            -reconfigure -backend-config="prefix=production" -- \
+        terraform -chdir=infra plan -out=tfplan
+}
+
+@test "overrides Google Cloud Service credentials" {
+    setup_remote_env
+    deploy <<< "y" --gcs-creds "/path/to/test_creds.json"
+    assert_equal $GOOGLE_CREDENTIALS "/path/to/test_creds.json"
+}
+
+@test "creates Terraform plan from environment variables" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=production" \
+        -var "app_package=cicd-repo" \
+        -var "namespace=cicd-account" \
+        -var "domain=example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates Terraform plan from options" {
+    setup_remote_env
+    run deploy <<< "y" \
+        --domain "example.test" \
+        --dns-zone "123abc" \
+        --ssh-port "2222" \
+        --ssh-key-id "2345" \
+        --email "test@example.com" \
+        --public-key "/path/to/test_key.pub" \
+        --cf-token "test_token" \
+        --do-token "test_token"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=production" \
+        -var "app_package=cicd-repo" \
+        -var "namespace=cicd-account" \
+        -var "domain=example.test" \
+        -var "dns_zone=123abc" \
+        -var "ssh_port=2222" \
+        -var "acme_email=test@example.com" \
+        -var "ssh_key_id=2345" \
+        -var "ssh_public_key_file=/path/to/test_key.pub" \
+        -var "cf_token=test_token" \
+        -var "do_token=test_token"
+}
+
+@test "creates Terraform plan before applying" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mocks_called_in_order \
+        terraform -chdir=infra plan -out=tfplan -- \
+        terraform -chdir=infra apply tfplan
+}
+
+@test "applies Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra apply tfplan
+}
+
+@test "aborts if plan not approved" {
+    setup_remote_env
+    run deploy <<< "n"
+    assert_success
+    assert_output --partial "Deployment aborted."
+    assert_mock_not_called terraform apply
+}
+
+@test "automatically approves plan" {
+    setup_remote_env
+    run deploy --approve
+    assert_success
+    assert_mock_called_once terraform -chdir=infra apply tfplan
 }