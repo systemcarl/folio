systemcarl/folio-deploy

Authenticate Docker with GitHub Packages
When a GitHub Personal Access Token (PAT) is provided for authentication
with the GitHub Packages container registry, automatically log in with
the application repository account and the provided token. Since the
authentication happens before every operation, it is still preferred to
instead authenticate the Docker engine manually before running commands.

cli/tests/containerize.bats
@@ -12,7 +12,13 @@ setup() {
     mock npm
 
     docker() {
-        log_mock_call docker "$@"
+        if [[ "$1" == "login" ]]; then
+            local input=""
+            if [ ! -t 0 ]; then
+                while IFS= read -r line; do input+="$line"$'\n'; done
+            fi
+        fi
+        log_mock_call docker "$@" "$input"
         if [[ "$1" == "image" && "$2" == "inspect" ]]; then
             if [[ $(get_mock_state image_local) == "false" ]]; then
                 return 1
@@ -57,6 +63,27 @@ teardown() {
     run containerize
     assert_success
     assert_mock_called_once get_version folio
+}
+
+@test "does not authenticate Docker" {
+    run containerize
+    assert_success
+    assert_mock_not_called docker login
+}
+
+@test "authenticates Docker with environment GitHub Packages token" {
+    FOLIO_GHPR_TOKEN="ghpr_test"
+    run containerize --push
+    assert_success
+    assert_mock_called_once docker login ghcr.io \
+        --username "app-account" --password-stdin "ghpr_test"
+}
+
+@test "authenticates Docker with GitHub Packages token option" {
+    run containerize --push --ghpr-token "ghpr_test"
+    assert_success
+    assert_mock_called_once docker login ghcr.io \
+        --username "app-account" --password-stdin "ghpr_test"
 }
 
 @test "fetches local image" {

systemcarl/folio

cli/tests/containerize.bats
@@ -12,7 +12,13 @@ setup() {
     mock npm
 
     docker() {
-        log_mock_call docker "$@"
+        if [[ "$1" == "login" ]]; then
+            local input=""
+            if [ ! -t 0 ]; then
+                while IFS= read -r line; do input+="$line"$'\n'; done
+            fi
+        fi
+        log_mock_call docker "$@" "$input"
         if [[ "$1" == "image" && "$2" == "inspect" ]]; then
             if [[ $(get_mock_state image_local) == "false" ]]; then
                 return 1
@@ -57,6 +63,27 @@ teardown() {
     run containerize
     assert_success
     assert_mock_called_once get_version app
+}
+
+@test "does not authenticate Docker" {
+    run containerize
+    assert_success
+    assert_mock_not_called docker login
+}
+
+@test "authenticates Docker with environment GitHub Packages token" {
+    FOLIO_GHPR_TOKEN="ghpr_test"
+    run containerize --push
+    assert_success
+    assert_mock_called_once docker login ghcr.io \
+        --username "cicd-account" --password-stdin "ghpr_test"
+}
+
+@test "authenticates Docker with GitHub Packages token option" {
+    run containerize --push --ghpr-token "ghpr_test"
+    assert_success
+    assert_mock_called_once docker login ghcr.io \
+        --username "cicd-account" --password-stdin "ghpr_test"
 }
 
 @test "fetches local image" {