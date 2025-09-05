systemcarl/folio-deploy

Deploy to Environment Domain
When deploying an environment other than `production`, prefix the
target domain with the environment as a subdomain. If the domain is
explicitly set, do not prefix with the environment.

cli/tests/deploy.bats
@@ -613,10 +613,67 @@ teardown() {
         -var "environment=staging" \
         -var "app_version=1.2.3" \
         -var "namespace=app-account" \
-        -var "domain=example.com" \
+        -var "domain=staging.example.com" \
         -var "dns_zone=abc123" \
         -var "ssh_port=22" \
         -var "acme_email=example@example.com" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates test Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y" --test
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_version=1.2.3" \
+        -var "namespace=app-account" \
+        -var "domain=test.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates environment Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y" --environment "env"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=env" \
+        -var "app_version=1.2.3" \
+        -var "namespace=app-account" \
+        -var "domain=env.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates Terraform plan with custom domain" {
+    setup_remote_env
+    run deploy <<< "y" --environment "test" --domain "example.test"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_version=1.2.3" \
+        -var "namespace=app-account" \
+        -var "domain=example.test" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
         -var "ssh_public_key_file=/path/to/public_key.pub" \
         -var "cf_token=cf_token" \
         -var "do_token=do_token"

cli/tests/destroy.bats
@@ -336,7 +336,64 @@ teardown() {
         -var "environment=staging" \
         -var "app_version=1.2.3" \
         -var "namespace=app-account" \
-        -var "domain=example.com" \
+        -var "domain=staging.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates test Terraform destroy plan" {
+    setup_remote_env
+    run destroy <<< "y" --test
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan -destroy \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_version=1.2.3" \
+        -var "namespace=app-account" \
+        -var "domain=test.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates environment Terraform destroy plan" {
+    setup_remote_env
+    run destroy <<< "y" --environment "env"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan -destroy \
+        -out=tfplan \
+        -var "environment=env" \
+        -var "app_version=1.2.3" \
+        -var "namespace=app-account" \
+        -var "domain=env.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates Terraform destroy plan with custom domain" {
+    setup_remote_env
+    run destroy <<< "y" --environment "test" --domain "example.test"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan -destroy \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_version=1.2.3" \
+        -var "namespace=app-account" \
+        -var "domain=example.test" \
         -var "dns_zone=abc123" \
         -var "ssh_port=22" \
         -var "acme_email=example@example.com" \

systemcarl/folio

cli/tests/deploy.bats
@@ -615,7 +615,67 @@ teardown() {
         -var "app_package=cicd-repo" \
         -var "app_version=1.2.3" \
         -var "namespace=cicd-account" \
-        -var "domain=example.com" \
+        -var "domain=staging.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates test Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y" --test
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
+        -var "namespace=cicd-account" \
+        -var "domain=test.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates environment Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y" --environment "env"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=env" \
+        -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
+        -var "namespace=cicd-account" \
+        -var "domain=env.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates Terraform plan with custom domain" {
+    setup_remote_env
+    run deploy <<< "y" --environment "test" --domain "example.test"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
+        -var "namespace=cicd-account" \
+        -var "domain=example.test" \
         -var "dns_zone=abc123" \
         -var "ssh_port=22" \
         -var "acme_email=example@example.com" \

cli/tests/destroy.bats
@@ -339,7 +339,67 @@ teardown() {
         -var "app_package=cicd-repo" \
         -var "app_version=1.2.3" \
         -var "namespace=cicd-account" \
-        -var "domain=example.com" \
+        -var "domain=staging.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates test Terraform destroy plan" {
+    setup_remote_env
+    run destroy <<< "y" --test
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan -destroy \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
+        -var "namespace=cicd-account" \
+        -var "domain=test.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates environment Terraform destroy plan" {
+    setup_remote_env
+    run destroy <<< "y" --environment "env"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan -destroy \
+        -out=tfplan \
+        -var "environment=env" \
+        -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
+        -var "namespace=cicd-account" \
+        -var "domain=env.example.com" \
+        -var "dns_zone=abc123" \
+        -var "ssh_port=22" \
+        -var "acme_email=example@example.com" \
+        -var "ssh_key_id=1234" \
+        -var "ssh_public_key_file=/path/to/public_key.pub" \
+        -var "cf_token=cf_token" \
+        -var "do_token=do_token"
+}
+
+@test "creates Terraform destroy plan with custom domain" {
+    setup_remote_env
+    run destroy <<< "y" --environment "test" --domain "example.test"
+    assert_success
+    assert_mock_called_once terraform -chdir=infra plan -destroy \
+        -out=tfplan \
+        -var "environment=test" \
+        -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
+        -var "namespace=cicd-account" \
+        -var "domain=example.test" \
         -var "dns_zone=abc123" \
         -var "ssh_port=22" \
         -var "acme_email=example@example.com" \