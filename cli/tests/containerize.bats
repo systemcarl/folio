systemcarl/folio-deploy

Ensure Application Image Available When Deploying
If the application image is not available locally or remotely,
respective to the desired deployment target, automatically execute
containerization script.

cli/tests/containerize.bats
@@ -10,14 +10,31 @@ setup() {
     setup_mocks
 
     mock npm
-    mock docker
-    mock load_env
+
+    docker() {
+        log_mock_call docker "$@"
+        if [[ "$1" == "image" && "$2" == "inspect" ]]; then
+            if [[ $(get_mock_state image_local) == "false" ]]; then
+                return 1
+            fi
+        elif [[ "$1" == "pull" ]]; then
+            if [[ $(get_mock_state "image_published") == "false" ]]; then
+                return 1
+            fi
+        fi
+    }
+    export -f docker
 
     get_version() {
         log_mock_call get_version "$@"
         echo "1.2.3"
     }
     export -f get_version
+
+    set_mock_state "image_local" "false"
+    set_mock_state "image_published" "false"
+
+    mock load_env
 
     export FOLIO_APP_ACCOUNT="app-account"
     export FOLIO_APP_REPO="app-repo"
@@ -39,11 +56,79 @@ teardown() {
     assert_mock_called_once get_version folio
 }
 
+@test "fetches local image" {
+    run containerize
+    assert_success
+    assert_mock_called_once docker image inspect \
+        "app-repo:1.2.3"
+}
+
+@test "doesn't rebuild if local image exists" {
+    set_mock_state "image_local" "true"
+    run containerize
+    assert_success
+    assert_output --partial \
+        "Docker image 'app-repo:1.2.3' already exists."
+    assert_mock_not_called npm install
+    assert_mock_not_called docker build
+}
+
+@test "fetches published image" {
+    set_mock_state "image_published" "true"
+    run containerize
+    assert_success
+    assert_mock_called_once docker pull \
+        "ghcr.io/app-account/app-repo:1.2.3"
+}
+
+@test "doesn't rebuild local image published" {
+    set_mock_state "image_published" "true"
+    run containerize
+    assert_success
+    assert_output --partial \
+        "Docker image 'ghcr.io/app-account/app-repo:1.2.3' already exists."
+    assert_mock_not_called npm install
+    assert_mock_not_called docker build
+}
+
+@test "does't publish image if already published" {
+    set_mock_state "image_published" "true"
+    run containerize --push
+    assert_success
+    assert_output --partial \
+        "Docker image 'ghcr.io/app-account/app-repo:1.2.3' already exists."
+    assert_mock_not_called docker push
+}
+
 @test "installs npm dependencies" {
     run containerize
     assert_success
     assert_mock_called_once npm install
     assert_mock_called_in_dir folio npm install
+}
+
+@test "rebuilds if local image exists" {
+    set_mock_state "image_local" "true"
+    run containerize --rebuild
+    assert_success
+    assert_mock_called_once npm install
+    assert_mock_called_once docker build
+}
+
+@test "rebuilds if local image published" {
+    set_mock_state "image_published" "true"
+    run containerize --rebuild
+    assert_success
+    assert_mock_called_once npm install
+    assert_mock_called_once docker build
+}
+
+@test "rebuilds and publishes if already published" {
+    set_mock_state "image_published" "true"
+    run containerize --rebuild --push
+    assert_success
+    assert_mock_called_once npm install
+    assert_mock_called_once docker build
 }
 
 @test "installs npm dependencies before testing" {

cli/tests/deploy.bats
@@ -21,6 +21,14 @@ setup() {
     }
     export -f docker
 
+    mock containerize
+
+    get_version() {
+        log_mock_call version "$@"
+        echo "1.2.3"
+    }
+    export -f get_version
+
     mock load_env
 
     export FOLIO_APP_ACCOUNT="app-account"
@@ -56,6 +64,30 @@ teardown() {
         "Application is running on http://localhost:3000"
 }
 
+@test "containerizes application locally" {
+    run deploy --local
+    assert_success
+    assert_mock_called_once containerize
+    assert_mock_not_called containerize --push
+}
+
+@test "returns non-zero exit code if local containerization fails" {
+    containerize() { log_mock_call containerize "$@"; return 1; }
+    run deploy --local
+    assert_failure
+}
+
+@test "containerizes application before deploying" {
+    run deploy --local
+    assert_success
+    assert_mocks_called_in_order \
+        containerize -- \
+        docker run -d \
+            --name "folio" \
+            -p "3000:3000" \
+            "app-repo:1.2.3"
+}
+
 @test "deploys local Docker image to local container" {
     set_mock_state has_image true
     run deploy --local
@@ -63,7 +95,7 @@ teardown() {
     assert_mock_called_once docker run -d \
         --name "folio" \
         -p "3000:3000" \
-        "app-repo:latest"
+        "app-repo:1.2.3"
 }
 
 @test "does not pull remote Docker image if local image exists" {
@@ -78,7 +110,7 @@ teardown() {
     run deploy  --local
     assert_success
     assert_mock_called_once docker pull \
-        "ghcr.io/app-account/app-repo:latest"
+        "ghcr.io/app-account/app-repo:1.2.3"
 }
 
 @test "pulls remote Docker image before deploying" {
@@ -86,11 +118,11 @@ teardown() {
     run deploy  --local
     assert_success
     assert_mocks_called_in_order \
-        docker pull "ghcr.io/app-account/app-repo:latest" -- \
+        docker pull "ghcr.io/app-account/app-repo:1.2.3" -- \
         docker run -d \
             --name "folio" \
             -p "3000:3000" \
-            "ghcr.io/app-account/app-repo:latest"
+            "ghcr.io/app-account/app-repo:1.2.3"
 }
 
 @test "deploys remote Docker image to local container" {
@@ -100,7 +132,7 @@ teardown() {
     assert_mock_called_once docker run -d \
         --name "folio" \
         -p "3000:3000" \
-        "ghcr.io/app-account/app-repo:latest"
+        "ghcr.io/app-account/app-repo:1.2.3"
 }
 
 @test "deploys remotely" {
@@ -212,6 +244,29 @@ teardown() {
     assert_output --partial "Deployment of production completed successfully."
 }
 
+@test "containerizes and publishes application" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once containerize --push
+}
+
+@test "returns non-zero exit code if containerization fails" {
+    setup_remote_env
+    containerize() { log_mock_call containerize "$@"; return 1; }
+    run deploy <<< "y"
+    assert_failure
+}
+
+@test "containerizes application before applying Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mocks_called_in_order \
+        containerize --push -- \
+        terraform apply tfplan
+}
+
 @test "initializes Terraform" {
     setup_remote_env
     run deploy <<< "y"
@@ -254,6 +309,7 @@ teardown() {
     assert_mock_called_in_dir infra terraform plan \
         -out=tfplan \
         -var "environment=production" \
+        -var "app_version=1.2.3" \
         -var "namespace=app-account" \
         -var "domain=example.com" \
         -var "dns_zone=abc123" \
@@ -278,6 +334,7 @@ teardown() {
     assert_mock_called_in_dir infra terraform plan \
         -out=tfplan \
         -var "environment=production" \
+        -var "app_version=1.2.3" \
         -var "namespace=app-account" \
         -var "domain=example.com" \
         -var "dns_zone=abc123" \
@@ -295,6 +352,7 @@ teardown() {
     assert_mock_called_in_dir infra terraform plan \
         -out=tfplan \
         -var "environment=staging" \
+        -var "app_version=1.2.3" \
         -var "namespace=app-account" \
         -var "domain=example.com" \
         -var "dns_zone=abc123" \

systemcarl/folio

cli/tests/containerize.bats
@@ -10,12 +10,27 @@ setup() {
     setup_mocks
 
     mock npm
-    mock docker
+
+    docker() {
+        log_mock_call docker "$@"
+        if [[ "$1" == "image" && "$2" == "inspect" ]]; then
+            if [[ $(get_mock_state image_local) == "false" ]]; then
+                return 1
+            fi
+        elif [[ "$1" == "pull" ]]; then
+            if [[ $(get_mock_state "image_published") == "false" ]]; then
+                return 1
+            fi
+        fi
+    }
 
     get_version() {
         log_mock_call get_version "$@"
         echo "1.2.3"
     }
+
+    set_mock_state "image_local" "false"
+    set_mock_state "image_published" "false"
 
     mock load_env
 
@@ -44,11 +59,79 @@ teardown() {
     assert_mock_called_once get_version app
 }
 
+@test "fetches local image" {
+    run containerize
+    assert_success
+    assert_mock_called_once docker image inspect \
+        "cicd-repo:1.2.3"
+}
+
+@test "doesn't rebuild if local image exists" {
+    set_mock_state "image_local" "true"
+    run containerize
+    assert_success
+    assert_output --partial \
+        "Docker image 'cicd-repo:1.2.3' already exists."
+    assert_mock_not_called npm install
+    assert_mock_not_called docker build
+}
+
+@test "fetches published image" {
+    set_mock_state "image_published" "true"
+    run containerize
+    assert_success
+    assert_mock_called_once docker pull \
+        "ghcr.io/cicd-account/cicd-repo:1.2.3"
+}
+
+@test "doesn't rebuild local image published" {
+    set_mock_state "image_published" "true"
+    run containerize
+    assert_success
+    assert_output --partial \
+        "Docker image 'ghcr.io/cicd-account/cicd-repo:1.2.3' already exists."
+    assert_mock_not_called npm install
+    assert_mock_not_called docker build
+}
+
+@test "does't publish image if already published" {
+    set_mock_state "image_published" "true"
+    run containerize --push
+    assert_success
+    assert_output --partial \
+        "Docker image 'ghcr.io/cicd-account/cicd-repo:1.2.3' already exists."
+    assert_mock_not_called docker push
+}
+
 @test "installs npm dependencies" {
     run containerize
     assert_success
     assert_mock_called_once npm install
     assert_mock_called_in_dir app npm install
+}
+
+@test "rebuilds if local image exists" {
+    set_mock_state "image_local" "true"
+    run containerize --rebuild
+    assert_success
+    assert_mock_called_once npm install
+    assert_mock_called_once docker build
+}
+
+@test "rebuilds if local image published" {
+    set_mock_state "image_published" "true"
+    run containerize --rebuild
+    assert_success
+    assert_mock_called_once npm install
+    assert_mock_called_once docker build
+}
+
+@test "rebuilds and publishes if already published" {
+    set_mock_state "image_published" "true"
+    run containerize --rebuild --push
+    assert_success
+    assert_mock_called_once npm install
+    assert_mock_called_once docker build
 }
 
 @test "installs npm dependencies before testing" {

cli/tests/deploy.bats
@@ -20,6 +20,13 @@ setup() {
         fi
     }
 
+    mock containerize
+
+    get_version() {
+        log_mock_call version "$@"
+        echo "1.2.3"
+    }
+
     mock load_env
 
     FOLIO_CICD_ACCOUNT="cicd-account"
@@ -56,6 +63,30 @@ teardown() {
     assert_mock_called_once load_env
 }
 
+@test "containerizes application locally" {
+    run deploy --local
+    assert_success
+    assert_mock_called_once containerize
+    assert_mock_not_called containerize --push
+}
+
+@test "returns non-zero exit code if local containerization fails" {
+    containerize() { log_mock_call containerize "$@"; return 1; }
+    run deploy --local
+    assert_failure
+}
+
+@test "containerizes application before deploying" {
+    run deploy --local
+    assert_success
+    assert_mocks_called_in_order \
+        containerize -- \
+        docker run -d \
+            --name "folio" \
+            -p "3000:3000" \
+            "cicd-repo:1.2.3"
+}
+
 @test "deploys local Docker image to local container" {
     set_mock_state has_image true
     run deploy --local
@@ -63,7 +94,7 @@ teardown() {
     assert_mock_called_once docker run -d \
         --name "folio" \
         -p "3000:3000" \
-        "cicd-repo:latest"
+        "cicd-repo:1.2.3"
 }
 
 @test "deploys local package" {
@@ -72,7 +103,7 @@ teardown() {
     run deploy --local
     assert_success
     assert_mock_called_once docker run -d \
-        "test-package:latest"
+        "test-package:1.2.3"
 }
 
 @test "does not pull remote Docker image if local image exists" {
@@ -87,7 +118,7 @@ teardown() {
     run deploy --local
     assert_success
     assert_mock_called_once docker pull \
-        "ghcr.io/cicd-account/cicd-repo:latest"
+        "ghcr.io/cicd-account/cicd-repo:1.2.3"
 }
 
 @test "pulls remote Docker image from namespace" {
@@ -96,7 +127,7 @@ teardown() {
     run deploy --local
     assert_success
     assert_mock_called_once docker pull \
-        "ghcr.io/test-account/cicd-repo:latest"
+        "ghcr.io/test-account/cicd-repo:1.2.3"
 }
 
 @test "pulls remote package" {
@@ -105,7 +136,7 @@ teardown() {
     run deploy --local
     assert_success
     assert_mock_called_once docker pull \
-        "ghcr.io/cicd-account/test-package:latest"
+        "ghcr.io/cicd-account/test-package:1.2.3"
 }
 
 @test "pulls remote Docker image before deploying" {
@@ -113,11 +144,11 @@ teardown() {
     run deploy --local
     assert_success
     assert_mocks_called_in_order \
-        docker pull "ghcr.io/cicd-account/cicd-repo:latest" -- \
+        docker pull "ghcr.io/cicd-account/cicd-repo:1.2.3" -- \
         docker run -d \
             --name "folio" \
             -p "3000:3000" \
-            "ghcr.io/cicd-account/cicd-repo:latest"
+            "ghcr.io/cicd-account/cicd-repo:1.2.3"
 }
 
 @test "deploys remote Docker image to local container" {
@@ -127,7 +158,7 @@ teardown() {
     assert_mock_called_once docker run -d \
         --name "folio" \
         -p "3000:3000" \
-        "ghcr.io/cicd-account/cicd-repo:latest"
+        "ghcr.io/cicd-account/cicd-repo:1.2.3"
 }
 
 @test "deploys remote package to local container" {
@@ -139,7 +170,7 @@ teardown() {
     assert_mock_called_once docker run -d \
         --name "folio" \
         -p "3000:3000" \
-        "ghcr.io/test-account/test-package:latest"
+        "ghcr.io/test-account/test-package:1.2.3"
 }
 
 @test "deploys remotely" {
@@ -285,6 +316,29 @@ teardown() {
     assert_output --partial "Deployment of production completed successfully."
 }
 
+@test "containerizes and publishes application" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mock_called_once containerize --push
+}
+
+@test "returns non-zero exit code if containerization fails" {
+    setup_remote_env
+    containerize() { log_mock_call containerize "$@"; return 1; }
+    run deploy <<< "y"
+    assert_failure
+}
+
+@test "containerizes application before applying Terraform plan" {
+    setup_remote_env
+    run deploy <<< "y"
+    assert_success
+    assert_mocks_called_in_order \
+        containerize --push -- \
+        terraform -chdir=infra apply tfplan
+}
+
 @test "initializes Terraform" {
     setup_remote_env
     run deploy <<< "y"
@@ -328,6 +382,7 @@ teardown() {
         -out=tfplan \
         -var "environment=production" \
         -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
         -var "namespace=cicd-account" \
         -var "domain=example.com" \
         -var "dns_zone=abc123" \
@@ -355,6 +410,7 @@ teardown() {
         -out=tfplan \
         -var "environment=production" \
         -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
         -var "namespace=cicd-account" \
         -var "domain=example.test" \
         -var "dns_zone=123abc" \
@@ -374,6 +430,7 @@ teardown() {
         -out=tfplan \
         -var "environment=staging" \
         -var "app_package=cicd-repo" \
+        -var "app_version=1.2.3" \
         -var "namespace=cicd-account" \
         -var "domain=example.com" \
         -var "dns_zone=abc123" \