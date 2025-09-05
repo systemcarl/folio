systemcarl/folio-deploy

Accept Environment Deployment Option

cli/tests/deploy.bats
@@ -521,6 +521,7 @@ teardown() {
 @test "creates Terraform plan from options" {
     setup_remote_env
     run deploy <<< "y" \
+        --environment "testing" \
         --domain "example.test" \
         --dns-zone "123abc" \
         --ssh-port "2222" \
@@ -532,7 +533,7 @@ teardown() {
     assert_success
     assert_mock_called_once terraform -chdir=infra plan \
         -out=tfplan \
-        -var "environment=production" \
+        -var "environment=testing" \
         -var "app_version=1.2.3" \
         -var "namespace=app-account" \
         -var "domain=example.test" \

cli/tests/destroy.bats
@@ -294,6 +294,7 @@ teardown() {
 @test "creates Terraform destroy plan from options" {
     setup_remote_env
     run destroy <<< "y" \
+        --environment "testing" \
         --domain "example.test" \
         --dns-zone 123abc \
         --ssh-port 2222 \
@@ -305,7 +306,7 @@ teardown() {
     assert_success
     assert_mock_called_once terraform -chdir=infra plan -destroy \
         -out=tfplan \
-        -var "environment=production" \
+        -var "environment=testing" \
         -var "app_version=1.2.3" \
         -var "namespace=app-account" \
         -var "domain=example.test" \

systemcarl/folio

cli/tests/deploy.bats
@@ -522,6 +522,7 @@ teardown() {
 @test "creates Terraform plan from options" {
     setup_remote_env
     run deploy <<< "y" \
+        --environment "test" \
         --domain "example.test" \
         --dns-zone "123abc" \
         --ssh-port "2222" \
@@ -533,7 +534,7 @@ teardown() {
     assert_success
     assert_mock_called_once terraform -chdir=infra plan \
         -out=tfplan \
-        -var "environment=production" \
+        -var "environment=test" \
         -var "app_package=cicd-repo" \
         -var "app_version=1.2.3" \
         -var "namespace=cicd-account" \

cli/tests/destroy.bats
@@ -295,6 +295,7 @@ teardown() {
 @test "creates Terraform destroy plan from options" {
     setup_remote_env
     run destroy <<< "y" \
+        --environment "test" \
         --domain "example.test" \
         --dns-zone 123abc \
         --ssh-port 2222 \
@@ -306,7 +307,7 @@ teardown() {
     assert_success
     assert_mock_called_once terraform -chdir=infra plan -destroy \
         -out=tfplan \
-        -var "environment=production" \
+        -var "environment=test" \
         -var "app_package=cicd-repo" \
         -var "app_version=1.2.3" \
         -var "namespace=cicd-account" \