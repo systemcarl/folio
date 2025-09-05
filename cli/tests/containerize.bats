systemcarl/folio-deploy

Test Application During Build & Containerization

cli/tests/containerize.bats
@@ -60,6 +60,21 @@ teardown() {
     assert_mock_called_in_dir folio npm install
 }
 
+@test "installs npm dependencies before testing" {
+    run containerize
+    assert_success
+    assert_mocks_called_in_order \
+        npm install -- \
+        npm run test
+}
+
+@test "runs tests" {
+    run containerize
+    assert_success
+    assert_mock_called_once npm run test
+    assert_mock_called_in_dir folio npm run test
+}
+
 @test "builds SvelteKit application" {
     run containerize
     assert_success

systemcarl/folio

cli/tests/containerize.bats
@@ -51,6 +51,21 @@ teardown() {
     assert_mock_called_in_dir app npm install
 }
 
+@test "installs npm dependencies before testing" {
+    run containerize
+    assert_success
+    assert_mocks_called_in_order \
+        npm install -- \
+        npm run test
+}
+
+@test "runs tests" {
+    run containerize
+    assert_success
+    assert_mock_called_once npm run test
+    assert_mock_called_in_dir app npm run test
+}
+
 @test "builds SvelteKit application" {
     run containerize
     assert_success